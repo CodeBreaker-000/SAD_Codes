fbd032668af1622f751bee746d84e198e8f37808
CodeBreaker-000 <mohammedabdulnaeem0.1.0@gmail.com>
2025-10-08 15:03:11 +0600

    first class

---
+++ first_class/first_code.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  // print("Hello World");
+
+  // num a = 10;
+  // num b = 10.5;
+
+  // print("Value of a: $a");
+  // print("Value of b: $b");
+
+
+  String str = "Hello";
+
+  print("value of str: $str");
+  print("value of str: " + str);
+
+  num a = 10.5;
+  num b = 20;
+  print("value: ${a + b} " + str);
+
+}
